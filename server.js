78ca3da0b5d2c4b59c205aaa2201abe250fc24eb
timothy okoduwa <71664736+timothy-okoduwa@users.noreply.github.com>
2025-07-11 11:57:17 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const { setupWSConnection } = require('y-websocket/bin/utils.js')
+const http = require('http')
+const WebSocket = require('ws')
+
+const port = process.env.PORT || 1234
+const host = '0.0.0.0'
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('okay')
+})
+
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', setupWSConnection)
+
+server.listen(port, host, () => {
+  console.log(`y-websocket server running at '${host}' on port ${port}`)
+})
